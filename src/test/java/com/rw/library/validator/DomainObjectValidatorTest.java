9a36e24933f6a6578fb31fded65afcc5889bd4a7
RobertWojtaszczyk <robert.wojtaszczyk@gmail.com>
2018-06-14 11:26:01 +0200

    unit tests validator

--- src/test/java/com/rw/library/validator/DomainObjectValidatorTest.java
+++ src/test/java/com/rw/library/validator/DomainObjectValidatorTest.java
@@ -1,7 +1,9 @@
 package com.rw.library.validator;
 
-import com.rw.library.domain.Book;
+import com.rw.library.domain.*;
+import com.rw.library.domain.definitions.Status;
 import com.rw.library.errors.DomainObjectNotFoundException;
+import com.rw.library.errors.EntityConstraintViolationException;
 import com.rw.library.service.BookServiceImpl;
 import com.rw.library.service.BorrowServiceImpl;
 import com.rw.library.service.CopyServiceImpl;
@@ -15,8 +17,6 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -37,30 +37,181 @@ public class DomainObjectValidatorTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
+    @Test
+    public void testValidateBookIdShouldNotThrowException() {
+        //Given
+        when(bookService.exists(1L)).thenReturn(true);
+        //When & Then
+        validator.validateBookId(1L);
+    }
 
     @Test
-    public void shouldThrowExceptionBookNotFound() {
+    public void testValidateBookIdShouldThrowExceptionBookNotFound() {
         //Given
-        thrown.expect(DomainObjectNotFoundException.class);
         when(bookService.exists(1L)).thenReturn(false);
+        thrown.expect(DomainObjectNotFoundException.class);
+        thrown.expectMessage("Could not find " + Book.class.toString() + " id '" + 1 + "'");
+        //When & Then
         validator.validateBookId(1L);
-        //Expect
-//        Throwable thrown = catchThrowable(() -> { validator.validateBookId(1L).throwAnException() });
-        assertThatThrownBy(() -> {throw new DomainObjectNotFoundException(Book.class.toString(), 1L);}).hasMessageContaining("Could not find");
-        //When
+    }
 
+    @Test
+    public void testValidateCopyIdShouldNotThrowException() {
+        //Given
+        when(copyService.exists(1L)).thenReturn(true);
+        //When & Then
+        validator.validateCopyId(1L);
+    }
+
+    @Test
+    public void testValidateCopyIdShouldThrowExceptionCopyNotFound() {
+        //Given
+        when(copyService.exists(1L)).thenReturn(false);
+        thrown.expect(DomainObjectNotFoundException.class);
+        thrown.expectMessage("Could not find " + Copy.class.toString() + " id '" + 1 + "'");
+        //When & Then
+        validator.validateCopyId(1L);
+    }
+
+    @Test
+    public void testValidateReaderIdShouldNotThrowException() {
+        //Given
+        when(readerService.exists(1L)).thenReturn(true);
+        //When & Then
+        validator.validateReaderId(1L);
+    }
+
+    @Test
+    public void testValidateReaderIdShouldThrowExceptionReaderNotFound() {
+        //Given
+        when(readerService.exists(1L)).thenReturn(false);
+        thrown.expect(DomainObjectNotFoundException.class);
+        thrown.expectMessage("Could not find " + Reader.class.toString() + " id '" + 1 + "'");
+        //When & Then
+        validator.validateReaderId(1L);
+    }
+
+    @Test
+    public void testValidateBorrowIdShouldNotThrowException() {
+        //Given
+        when(borrowService.exists(1L)).thenReturn(true);
+        //When & Then
+        validator.validateBorrowId(1L);
+    }
+
+    @Test
+    public void testValidateBorrowIdShouldThrowExceptionBorrowNotFound() {
+        //Given
+        when(borrowService.exists(1L)).thenReturn(false);
+        thrown.expect(DomainObjectNotFoundException.class);
+        thrown.expectMessage("Could not find " + Borrow.class.toString() + " id '" + 1 + "'");
+        //When & Then
+        validator.validateBorrowId(1L);
+    }
+
+    @Test
+    public void testValidateCopyShouldNotThrowAnyException() {
+        //Given
+        CopyDto copyDto = new CopyDto();
+        copyDto.setBookId(1L);
+        copyDto.setStatus(Status.OK);
+        when(bookService.exists(1L)).thenReturn(true);
+        //When & Then
+        validator.validateCopy(copyDto);
+    }
+
+    @Test
+    public void testValidateCopyShouldThrowExceptionBookIdCanNotBeNull() {
+        //Given
+        CopyDto copyDto = new CopyDto();
+        thrown.expect(EntityConstraintViolationException.class);
+        thrown.expectMessage(Copy.class.toString() + ": Book Id can not be null!");
+        //When & Then
+        validator.validateCopy(copyDto);
+    }
+
+    @Test
+    public void testValidateCopyShouldThrowExceptionBookNotFound() {
+        //Given
+        CopyDto copyDto = new CopyDto();
+        copyDto.setBookId(1L);
+        when(bookService.exists(1L)).thenReturn(false);
+        thrown.expect(DomainObjectNotFoundException.class);
+        thrown.expectMessage("Could not find " + Book.class.toString() + " id '" + 1 + "'");
+        //When & Then
+        validator.validateCopy(copyDto);
+    }
+
+    @Test
+    public void testValidateCopyShouldThrowExceptionWrongCopyStatus() {
+        //Given
+        CopyDto copyDto = new CopyDto();
+        copyDto.setBookId(1L);
+        when(bookService.exists(1L)).thenReturn(true);
+        thrown.expect(EntityConstraintViolationException.class);
+        thrown.expectMessage(Copy.class.toString() + ": wrong name of copy status!");
+        //When & Then
+        validator.validateCopy(copyDto);
+    }
+
+    @Test
+    public void testValidateBorrowShouldNotThrowException() {
+        //Given
+        BorrowDto borrowDto = new BorrowDto();
+        borrowDto.setCopyId(1L);
+        borrowDto.setReaderId(1L);
+        when(copyService.exists(1L)).thenReturn(true);
+        when(readerService.exists(1L)).thenReturn(true);
+        //When & Then
+        validator.validateBorrow(borrowDto);
+    }
+
+    @Test
+    public void testValidateBorrowShouldThrowExceptionCopyIdCanNotBeNull() {
+        //Given
+        BorrowDto borrowDto = new BorrowDto();
+        thrown.expect(EntityConstraintViolationException.class);
+        thrown.expectMessage(Borrow.class.toString() + ": Copy Id can not be null!");
+        //When & Then
+        validator.validateBorrow(borrowDto);
+    }
+
+    @Test
+    public void testValidateBorrowShouldThrowExceptionReaderIdCanNotBeNull() {
+        //Given
+        BorrowDto borrowDto = new BorrowDto();
+        borrowDto.setCopyId(1L);
+        thrown.expect(EntityConstraintViolationException.class);
+        thrown.expectMessage(Borrow.class.toString() + ": Reader Id can not be null!");
+        //When & Then
+        validator.validateBorrow(borrowDto);
+    }
+
+    @Test
+    public void testValidateBorrowShouldThrowExceptionCopyNotFound() {
+        //Given
+        BorrowDto borrowDto = new BorrowDto();
+        borrowDto.setCopyId(1L);
+        borrowDto.setReaderId(1L);
+        when(copyService.exists(1L)).thenReturn(false);
+        when(readerService.exists(1L)).thenReturn(false);
+        thrown.expect(DomainObjectNotFoundException.class);
+        thrown.expectMessage("Could not find " + Copy.class.toString() + " id '" + 1 + "'");
+        //When & Then
+        validator.validateBorrow(borrowDto);
+    }
+
+    @Test
+    public void testValidateBorrowShouldThrowExceptionReaderNotFound() {
+        //Given
+        BorrowDto borrowDto = new BorrowDto();
+        borrowDto.setCopyId(1L);
+        borrowDto.setReaderId(1L);
+        when(copyService.exists(1L)).thenReturn(true);
+        when(readerService.exists(1L)).thenReturn(false);
+        thrown.expect(DomainObjectNotFoundException.class);
+        thrown.expectMessage("Could not find " + Reader.class.toString() + " id '" + 1 + "'");
+        //When & Then
+        validator.validateBorrow(borrowDto);
     }
 }
-
-
-//Given
-
-//When
-
-//Then
-
-//Given
-
-//Expect
-
-//When
